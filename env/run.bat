104591feec491d4b03f979dd8bb3e74296f15a39
Oriol3 <iverpierce@gmail.com>
2024-01-18 19:00:38 +0100

    remove processes

--- env/run.bat
+++ env/run.bat
@@ -1,2 +1,2 @@
 cd C:\Users\lidia\oriol-molist\RingZero
-python -m pabot.pabot --processes 4 --outputdir results tests\android_login.robot
+python -m pabot.pabot --outputdir results tests\android_login.robot
